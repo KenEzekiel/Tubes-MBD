0f4e4ca5683582191589c02e5419c019db1dbc03
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-11-14 14:27:13 +0700

    add: resource and transaction list

--- src/Algorithm.py
+++ src/Algorithm.py
@@ -6,9 +6,27 @@ from Transaction import Transaction
 
 class Algorithm:
   schedule: Schedule
+  transactions: list
+  resources: list
 
   def __init__(self, schedule: Schedule):
     self.schedule = schedule
+    self.transactions = []
+    self.resources = []
+    list_transactions_id = []
+    list_resources_name = []
+
+    for i in self.schedule.operations:
+      if i.transaction_id in list_transactions_id or i.transaction_id == "":
+        pass
+      else:
+        self.transactions.append(Transaction(i.transaction_id))
+        list_transactions_id.append(i.transaction_id)
+      if i.resource_name in list_resources_name or i.resource_name == "":
+        pass
+      else:
+        self.resources.append(Resource(i.resource_name))
+        list_resources_name.append(i.resource_name)
 
   def rollback(self, trans: Transaction):
     for i in trans.operations_done:
@@ -18,4 +36,13 @@ class Algorithm:
   @abstractmethod  
   def execute(self):
     # Write the output to an output file (from user input)
-    pass
+    print("------------  LOG  ------------")
+
+  def __str__(self):
+    string = "----------\nAlgorithm\n----------\nTransactions:\n"
+    for i in self.transactions:
+      string += str(i) + "\n"
+    string += "Resources:\n"
+    for i in self.resources:
+      string += str(i) + "\n"
+    return string
