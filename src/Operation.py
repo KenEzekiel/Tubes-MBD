7be8a353873382a90804ced5b36ce2a3380c4a02
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-11-24 09:20:03 +0700

    add: error handling

--- src/Operation.py
+++ src/Operation.py
@@ -18,34 +18,38 @@ class Operation:
 
   def __init__(self, string: str):
     # Translate string to Operation
-    if string[0] == 'R':
-      self.op_type = Operation_Type.READ
-      self.transaction_id = int(string[1])
-      self.resource_name = string[3]
-    elif string[0] == 'W':
-      self.op_type = Operation_Type.WRITE
-      self.transaction_id = int(string[1])
-      self.resource_name = string[3]
-    elif string[0] == 'C':
-      self.op_type = Operation_Type.COMMIT
-      self.transaction_id = int(string[1])
-      self.resource_name = ""
-    elif string[0] == 'X':
-      self.op_type = Operation_Type.XLOCK
-      self.transaction_id = int(string[2])
-      self.resource_name = string[4]
-    elif string[0] == 'S':
-      self.op_type = Operation_Type.SLOCK
-      self.transaction_id = int(string[2])
-      self.resource_name = string[4]
-    elif string[0] == 'U':
-      self.op_type = Operation_Type.UNLOCK
-      self.transaction_id = "" # type: ignore
-      self.resource_name = string[3]
-    elif string[0] == 'V':
-      self.op_type = Operation_Type.VALIDATE
-      self.transaction_id = int(string[1])
-      self.resource_name = ""
+    try:
+      if string[0] == 'R':
+        self.op_type = Operation_Type.READ
+        self.transaction_id = int(string[1])
+        self.resource_name = string[3]
+      elif string[0] == 'W':
+        self.op_type = Operation_Type.WRITE
+        self.transaction_id = int(string[1])
+        self.resource_name = string[3]
+      elif string[0] == 'C':
+        self.op_type = Operation_Type.COMMIT
+        self.transaction_id = int(string[1])
+        self.resource_name = ""
+      elif string[0] == 'X':
+        self.op_type = Operation_Type.XLOCK
+        self.transaction_id = int(string[2])
+        self.resource_name = string[4]
+      elif string[0] == 'S':
+        self.op_type = Operation_Type.SLOCK
+        self.transaction_id = int(string[2])
+        self.resource_name = string[4]
+      elif string[0] == 'U':
+        self.op_type = Operation_Type.UNLOCK
+        self.transaction_id = "" # type: ignore
+        self.resource_name = string[3]
+      elif string[0] == 'V':
+        self.op_type = Operation_Type.VALIDATE
+        self.transaction_id = int(string[1])
+        self.resource_name = ""
+    except:
+      print("===== Invalid input! =====")
+    
 
   def __str__(self):
     return f'{self.op_type.name} {self.transaction_id} {self.resource_name}'
