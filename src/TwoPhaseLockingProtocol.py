64d52fdf5403f554c82cf05b8b1badd86339eef1
Nicholas Liem <13521135@std.stei.itb.ac.id>
2023-12-01 21:02:22 +0700

    feat: finalization 2pl

--- src/TwoPhaseLockingProtocol.py
+++ src/TwoPhaseLockingProtocol.py
@@ -12,6 +12,7 @@ class TwoPhaseLockingProtocol(Algorithm):
         self.name = "Strict Two Phase Locking Protocol"
         self.transaction_timestamps = {t.id: datetime.now() for t in self.transactions}
         self.waiting_queues = defaultdict(deque)
+        self.waiting_operations_queue = deque()
         self.retry_queue = deque()
         self.transaction_map = {t.id: i for i, t in enumerate(self.transactions)}
 
@@ -35,6 +36,9 @@ class TwoPhaseLockingProtocol(Algorithm):
         transaction = self.transactions[tx_index]
         resource = self.resources[(ord(operation.resource_name) - 65)] if operation.resource_name != "" else None
 
+        if transaction.status == Transaction.WAITING:
+            return
+
         if operation.op_type in [Operation_Type.READ, Operation_Type.WRITE]:
             lock_success = transaction.x_lock(resource) if resource else False
 
@@ -51,9 +55,15 @@ class TwoPhaseLockingProtocol(Algorithm):
             self.commit_transaction(transaction.id)
 
     def handle_lock_conflict(self, tx_id, conflicting_tx_id, res_name):
-        if self.transaction_timestamps[tx_id] < self.transaction_timestamps[conflicting_tx_id]:
-            self.waiting_queues[res_name].append(tx_id)
+        tx_index = self.transaction_map[tx_id]
+        transaction = self.transactions[tx_index]
+        if self.transaction_timestamps[tx_id] > self.transaction_timestamps[conflicting_tx_id]:
             self.transactions[self.transaction_map[tx_id]].status = Transaction.WAITING
+            for op in self.schedule.operations:
+                if op.transaction_id == tx_id:
+                    if [op.op_type.name, op.resource_name] not in transaction.operations_done:
+                        self.waiting_queues[res_name].append(op)
+
             string = f"Transaction {tx_id} will wait for resource {res_name}. (Wait-Die)"
         else:
             self.abort_transaction(tx_id)
@@ -63,12 +73,32 @@ class TwoPhaseLockingProtocol(Algorithm):
     def commit_transaction(self, tx_id):
         tx_index = self.transaction_map[tx_id]
         transaction = self.transactions[tx_index]
+        locked_resources = list(transaction.x_locked)
+        print("LOCKED RESOURCE")
+        print(locked_resources)
+
+        self.transaction_timestamps[tx_id] = datetime.now()
 
         ret = transaction.commit()
         super().write(ret)
 
         # Remove operations from retry_queue that belong to the committed transaction
         self.retry_queue = deque(op for op in self.retry_queue if op.transaction_id != tx_id)
+
+        for resource in locked_resources:
+            if resource.name in self.waiting_queues:
+                while self.waiting_queues[resource.name]:
+                    waiting_operation = self.waiting_queues[resource.name].popleft()
+                    waiting_tx_index = self.transaction_map[waiting_operation.transaction_id]
+                    waiting_transaction = self.transactions[waiting_tx_index]
+
+                    # Update the waiting transaction's status to ACTIVE
+                    if waiting_transaction.status == Transaction.WAITING:
+                        waiting_transaction.status = Transaction.ACTIVE
+
+                    # Process the operation if the transaction is not aborted
+                    if waiting_transaction.status != Transaction.ABORTED:
+                        self.process_operation(waiting_operation)
 
         self.process_retry_queue()
 
@@ -80,16 +110,13 @@ class TwoPhaseLockingProtocol(Algorithm):
     def abort_transaction(self, tx_id):
         tx_index = self.transaction_map[tx_id]
         transaction = self.transactions[tx_index]
+
+        self.transaction_timestamps[tx_id] = datetime.now()
         transaction.abort()
 
         for op in self.schedule.operations:
             if op.transaction_id == tx_id:
                 self.retry_queue.append(op)
-
-        # print("RETRY QUEUE: ")
-        # for operation in self.retry_queue:
-        #     print(operation)
-        # print("END")
 
 
 inputFile = input("Input file: ")
